10832d625edc5ea0d7633235d150ac649ccb6cd4
andre.trigo <andre.trigo@voraztecnologia.com>
2023-10-04 19:19:05 -0300

    Inicio Projeto Teste Paradigma

---
+++ TesteParadigma/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas através do seguinte
+// conjunto de atributos a seguir. Altere esses valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("TesteParadigma")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TesteParadigma")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como falso torna não visíveis os tipos neste assembly
+// para componentes COM. Caso precise acessar um tipo neste assembly a partir de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// A GUID a seguir será referente à ID do typelib se este projeto for exposto ao COM
+[assembly: Guid("66446e68-db1e-4b55-aa3b-e8a5dec80fd7")]
+
+// As informações de versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Major Version
+//      Versão Secundária
+//      Build Number
+//      Revision
+//
+// É possível especificar todos os valores ou definir como padrão os números de revisão e de versão
+// usando o '*' como mostrado abaixo:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
