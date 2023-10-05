c2b8dfe360958e5fde5c24b793b8f2768db1e785
andre.trigo <andre.trigo@voraztecnologia.com>
2023-10-05 00:27:22 -0300

    Conclusão tarefa 1

--- TesteParadigma/Controllers/HomeController.cs
+++ TesteParadigma/Controllers/HomeController.cs
@@ -15,14 +15,26 @@ namespace TesteParadigma.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.Message = "Informações sobre o André";
+            ViewBag.Resumo = "Atuando no setor de Desenvolvimento de Software Web e Mobile desde Junho de 2020, no segmento de restaurantes e delivery (food service), " +
+                "como desenvolvedor Full-Stack Pleno, auxiliando na automação e gestão deste setor, utilizando as tecnologias .NET C# e JavaScript.\r\n\r\n" +
+                "Atuei como desenvolvedor .NET C# em SAP Business One, pela Voraz Consultoria durante 3 meses em 2022";
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Nome = "André Luís Trigo Fernandes";
+            ViewBag.Telefone = "(46) 999147928";
+            ViewBag.Endereco = "Rua Doutor Francisco Beltrão 360";
+            ViewBag.Bairro = "Santa Teresinha";
+            ViewBag.Complemento = "Apto 218";
+            ViewBag.Cidade = "Pato Branco";
+            ViewBag.Email = "andrefernandes@alunos.utfpr.edu.br";
+            ViewBag.Estado = "Paraná - PR";
+            ViewBag.Git = "https://github.com/Andre13trigo/";
+
+
 
             return View();
         }
---
+++ TesteParadigma/Controllers/TarefasController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TesteParadigma.Controllers
+{
+    public class TarefasController : Controller
+    {
+        // GET: Tarefas
+        public ActionResult Tarefa1()
+        {
+            ViewBag.Enunciado = "Utilizando o array [7, 5, 3, 9, 6, 4, 1], faça o código para percorrer a lista, localizar e substituir o valor 9 por 5,\r\nremover o " +
+                "valor 4 da lista. Ao final você deve apresentar na tela a lista original, a nova lista e a soma dos valores da\r\nnova lista.";
+            return View();
+        }
+
+        public ActionResult Tarefa2()
+        {
+            return View();
+        }
+
+        public ActionResult Tarefa3()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult ResolveTarefa1(string[] parametro)
+        {
+            int count = 0;
+            List<string> aux = new List<string>();
+
+            foreach (var item in parametro)
+            {
+                if (item == "9")
+                {
+                    aux.Add("5");
+                }
+                else if (item == "4")
+                {
+                    aux.Add("0");
+                }
+                else
+                {
+                    aux.Add(item);
+                }
+                count++;
+            }
+            aux = RemoverZeros(aux.ToArray());
+
+            int numero;
+            int soma = 0;
+            foreach (var item in aux)
+            {
+                if (int.TryParse(item, out numero))
+                {
+                    soma += numero;
+                }
+            }
+            return Json(new { parametro, aux, soma });
+        }
+
+        public static List<string> RemoverZeros(string[] vetor)
+        {
+            vetor = vetor.Where(num => num != "0").ToArray();
+            return vetor.ToList();
+        }
+    }
+}
