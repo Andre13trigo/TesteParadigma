e7d8cbf048b2031f6171190ed3170e0cd0b4fbbb
andre.trigo <andre.trigo@voraztecnologia.com>
2023-10-05 20:10:04 -0300

    finalização tarefa 3

---
+++ TesteParadigma/Models/ModelTask3.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TesteParadigma.Models
+{
+    public class ModelTask3
+    {
+        public string array { get; set; }
+        public int soma { get; set;}
+    }
+}
